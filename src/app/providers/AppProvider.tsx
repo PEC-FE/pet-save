99641e727e2a788c697cf3616baa61e31d38060a
bytrustu <bytrustu@gmail.com>
2024-08-04 23:32:32 +0900

    feat: QueryClientProvider, ChakraProvider 추가

--- src/app/providers/AppProvider.tsx
+++ src/app/providers/AppProvider.tsx
@@ -1,1 +1,26 @@
-export const ThemeProvider = () => {};
+'use client';
+
+import { ReactNode } from 'react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { getQueryClient } from '@/src/shared';
+
+type Props = {
+  children: ReactNode;
+};
+
+export const AppProvider = ({ children }: Props) => {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+};
