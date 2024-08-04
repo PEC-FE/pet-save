f4b0d222eeb186612da094a66ae29103ee132ceb
bluel00 <ickyicky@gmail.com>
2024-08-04 11:39:26 +0900

    보호소 목록페이지에 필요한 entity 추가

--- src/entities/shelter/model/shelter.ts
+++ src/entities/shelter/model/shelter.ts
@@ -1,1 +1,41 @@
-export type ShelterModel = {};
+// TODO-cch: type 한곳으로 옮기기
+
+export type ShelterType = 'animalHospital' | 'shelter' | 'rescueGroup';
+export type AnimalType = 'cat' | 'dog' | 'other';
+
+export interface Shelter {
+  name: string;
+  type: string;
+  animalType: string;
+  address: string;
+  phoneNumber: string;
+  operatingHours: string;
+}
+
+export interface Pagination {
+  totalCount: number;
+  currentPage: number;
+  pageSize: number;
+  totalPages: number;
+}
+
+export interface Shelters {
+  shelters: Shelter[];
+  pagination: Pagination;
+}
+
+export const createShelter = (
+  name: string,
+  type: string,
+  animalType: string,
+  address: string,
+  phoneNumber: string,
+  operatingHours: string,
+): Shelter => ({
+  name,
+  type,
+  animalType,
+  address,
+  phoneNumber,
+  operatingHours,
+});
