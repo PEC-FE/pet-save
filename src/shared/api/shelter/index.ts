febff43f079185e5b687a48a68db858957751966
bytrustu <bytrustu@gmail.com>
2024-08-08 19:45:07 +0900

    feat: 봉사자 리스트, 상세정보 api, query, mapper 추가

--- src/shared/api/shelter/index.ts
+++ src/shared/api/shelter/index.ts
@@ -4,7 +4,5 @@
 
 export * from './mapToShelterDetail';
 export * from './mapToShelterList';
-export * from './mapToVolunteerDetail';
-export * from './mapToVolunteerList';
 export * from './postShelterLikeFavorite.api';
 export * from './type';
