5dae9ba03e8208f62fd7b330afd93689825d9e6d
bluel00 <ickyicky@gmail.com>
2024-08-14 07:39:36 +0900

    보호소목록 데이터 가져오는 부분 커스텀 훅으로 분리

--- src/widgets/shelter/lib/useShelterList.ts
+++ src/widgets/shelter/lib/useShelterList.ts
@@ -1,1 +1,22 @@
-export const useShelterList = () => {};
+import { useState, useEffect } from 'react';
+
+import { Shelters } from '@/src/entities';
+import { mapToShelters } from '@/src/shared/api/shelter/mapToShelters';
+
+import { mockData } from '@/app/shelters/__mockData__';
+
+const getShelters = () => {
+  return mockData;
+};
+
+export const useShelterList = () => {
+  const [sheltersData, setSheltersData] = useState<Shelters | null>(null);
+
+  useEffect(() => {
+    const apiResponse = getShelters();
+    const mappedData: Shelters = mapToShelters(apiResponse);
+    setSheltersData(mappedData);
+  }, []);
+
+  return { sheltersData };
+};
